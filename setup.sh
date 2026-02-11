c285d1b5692eaef357cbf879ad7f91bc1c5d2a76
crb1-hub <carlos.barajas@jhuapl.edu>
2026-02-10 20:32:27 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ mkdir -p ironclad/storage
 
 echo " > images and probe"
 wget --no-check-certificate \
-  "https://www.dropbox.com/scl/fi/2xt7815a9un3daau68ns4/multi_image_identities.tar?rlkey=68oj82dqyhj0aa7unbuyo3uc9&st=vtl5s0da&dl=1" \
+  "https://livejohnshopkins-my.sharepoint.com/:u:/g/personal/jpulido6_jh_edu/IQAhv09T__00RqMeUDkTC8Y4AYiaEwHhDKJtiN16q0o0Ke4?e=b4FzmU&download=1" \
   -O ironclad/storage/multi_image_identities.tar
 
 echo " > Extracting..."
